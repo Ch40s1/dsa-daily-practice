80c2a5e6a750f78a578a94ecc87ee2489bd2546c
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2024-05-15 21:18:18 -0500

    added basic questions

---
+++ src/questions.py
@@ -0,0 +1,32 @@
+quiz_questions = {
+    1: {
+        "question": "What command is used to list the contents of a directory in Unix?",
+        "options": {
+            "A": "ls -l",
+            "B": "dir",
+            "C": "list",
+            "D": "show"
+        },
+        "correct_answer": "A"
+    },
+    2: {
+        "question": "Which command is used to create a new directory in Unix?",
+        "options": {
+            "A": "mkdir",
+            "B": "newdir",
+            "C": "mkd",
+            "D": "makefolder"
+        },
+        "correct_answer": "A"
+    },
+    3: {
+        "question": "What command is used to display the contents of a file in Unix?",
+        "options": {
+            "A": "cat",
+            "B": "view",
+            "C": "display",
+            "D": "showfile"
+        },
+        "correct_answer": "A"
+    }
+}
