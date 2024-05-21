747f71bcd9e9ea428cbb036b68983bfa10ff9164
Ch40s1 <114247747+Ch40s1@users.noreply.github.com>
2024-05-20 20:16:52 -0500

    added new main file contents

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-# dsa-daily-practice 
+# dsa-daily-practice
 
 I need a way to study data structures and algorithms. This project will be a study site. It will have questions to help study.
+For now it gives a question you have to conplete and then it compares the file given to the answer file.
 
 ## Usage
 Clone repo or download file
 Run ``` python src/main.py or python3 src/main.py ```
 
-### Future plans 
+### Future plans
 Make it into a website for studying
--- src/main.py
+++ src/main.py
@@ -1,35 +1,11 @@
-from questions import quiz_questions
-
-
 def main():
-
-    def run_course():
-        while True:
-            user_program()
-
-    def user_program():
-        user_answer = None
-        print("HELOOOOOOO! Welcome")
-        for q_id, q_info in quiz_questions.items():
-            print(f"questions {q_id}: {q_info['question']}")
-            correct_answer = q_info["correct_answer"]
-            for option, answer in q_info['options'].items():
-                print(f"  {option}) {answer}")
-            user_answer = input("Choose answer: ").lower()
-            if user_answer == correct_answer:
-                print("Correct!")
-                continue
-            else:
-                print("Incorrect")
-                print(f"correct answer was: {correct_answer}")
-                continue
-        prompt_program = input("Type 'exit' to exit?\n").lower()
-        if prompt_program == "exit":
-            print("exiting program")
-            exit()
-
-    run_course()
-
+    print("Hello, World!")
+    # create two variables to hold the paths to the two files
+    # give some massage to the user
+    # open the first file in read mode
+    # open the second file in read mode
+    # check if the files are the same
+    # if they are the same, print a message to the user
 
 if __name__ == "__main__":
     main()
